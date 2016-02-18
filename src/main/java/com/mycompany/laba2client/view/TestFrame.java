02c523362d85d9ba05f801b5d4b5babceb778706
masteryodo <fotogosu@gmail.com>
2016-02-18 15:55:03 +0400

    Все почти осталось переброс с сервера на сервер сделать и комаратор

--- src/main/java/com/mycompany/laba2client/view/TestFrame.java
+++ src/main/java/com/mycompany/laba2client/view/TestFrame.java
@@ -2,14 +2,13 @@
 package com.mycompany.laba2client.view;
 
 import static com.mycompany.laba2client.Constants.*;
-import com.mycompany.laba2client.LifeCycle;
 import com.mycompany.laba2client.controller.ClientController;
 import com.mycompany.laba2client.dto.Client;
 import com.mycompany.laba2client.dto.Order;
+import com.mycompany.laba2client.utils.Finder;
 import com.mycompany.laba2client.utils.XmlReaderWriter;
 import java.util.HashSet;
 import com.mycompany.laba2client.utils.GenerateId;
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.logging.Level;
@@ -24,34 +23,30 @@ public final class TestFrame extends javax.swing.JFrame {
     private final SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
     private ClientsTableModel clients_model;
     private OrdersTableModel orders_model;
-    
-    
+    private Finder finder;
 
     public TestFrame(ClientController controller) {
         
         this.clients = xml.readClientsFromXml(CLIENTS_FILE);
         this.orders = xml.readOrdersFromXml(ORDERS_FILE);
         initComponents();
-       
-       this.controller = controller;
+        this.finder = new Finder();
+        this.controller = controller;
        
        refreshClientsTableModel();
-       OrdersTableModel orders_model = new OrdersTableModel(orders, controller);
-       jTable2.setModel(orders_model);
-       
+       refreshOrdersTableModel();
     }
     public void refreshClientsTableModel(){
         clients_model = new ClientsTableModel(clients, controller, this);
         jTable1.setModel(clients_model);
-        jTable1.revalidate();
+        //jTable1.revalidate();
     }
     
     public void refreshOrdersTableModel(){
         orders_model = new OrdersTableModel(orders, controller);
         jTable2.setModel(orders_model);
     }
-    
-    //@SuppressWarnings("unchecked")
+     //@SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -67,6 +62,8 @@ public final class TestFrame extends javax.swing.JFrame {
         jTable2 = new javax.swing.JTable();
         jTextField2 = new javax.swing.JTextField();
         jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -79,6 +76,11 @@ public final class TestFrame extends javax.swing.JFrame {
         });
 
         jButton1.setText("Найти");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Добавить клиента");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -134,9 +136,26 @@ public final class TestFrame extends javax.swing.JFrame {
         ));
         jScrollPane3.setViewportView(jTable2);
 
-        jTextField2.setText("jTextField2");
-
         jButton6.setText("Найти ордер");
+        jButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton6ActionPerformed(evt);
+            }
+        });
+
+        jButton7.setText("сбросить");
+        jButton7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton7ActionPerformed(evt);
+            }
+        });
+
+        jButton8.setText("сбросить");
+        jButton8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton8ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -155,12 +174,14 @@ public final class TestFrame extends javax.swing.JFrame {
                             .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 558, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButton5, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButton4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButton3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 157, Short.MAX_VALUE)
+                    .addComponent(jButton6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(59, 59, 59))
         );
         layout.setVerticalGroup(
@@ -170,43 +191,46 @@ public final class TestFrame extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jTextField1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(37, 37, 37)
+                        .addComponent(jButton7)
+                        .addGap(12, 12, 12)
                         .addComponent(jButton2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton3)
-                        .addGap(100, 100, 100))
+                        .addGap(111, 111, 111))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(11, 11, 11)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(11, 11, 11)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton6))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton8)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButton4)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton5)
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(41, Short.MAX_VALUE))))
+                        .addContainerGap(33, Short.MAX_VALUE))))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        //Добавление клиента
         clients.add(new Client(GenerateId.getId(), "", "", ""));
         refreshClientsTableModel();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        try {
+        try {// Добавление ордера
             // Если поле client_id не будет исправлено на действующее значение клиент не запищется.
             orders.add(new Order(GenerateId.getId(), new Long("0"), format.parse("01.01.1900"), new Double("0")) );
             refreshOrdersTableModel();
@@ -218,21 +242,27 @@ public final class TestFrame extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // Удаление клиента
         int row = jTable1.getSelectedRow();
-        Client client = getClientById((Long) clients_model.getValueAt(row, 0));
+        Client client = getClientById( (Long) clients_model.getValueAt(row, 0));
         try {
             controller.sendClientUpdates("remove", client);
-            jTable1.removeRowSelectionInterval(row, row);
+            clients = xml.readClientsFromXml(CLIENTS_FILE);
             refreshClientsTableModel();
         } catch (Exception ex) {
             Logger.getLogger(TestFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
+        refreshClientsTableModel();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         // Удалить ордер
-        Order order = getOrderById((Long) orders_model.getValueAt(jTable2.getSelectedRow(), 0));
+        int row = jTable2.getSelectedRow();
+        System.out.println(orders_model.getValueAt(row, 1));
+        long order_id = (Long) orders_model.getValueAt(row, 0);
+        System.out.println(order_id);
+        Order order = getOrderById(order_id);
         try {
             controller.sendOrderUpdates("remove", order);
+            orders = xml.readOrdersFromXml(ORDERS_FILE);
             refreshOrdersTableModel();
         } catch (Exception e) {
             System.out.println("remove action: " + e );
@@ -252,6 +282,32 @@ public final class TestFrame extends javax.swing.JFrame {
                 controller.closeSocket();
                 System.exit(0);
     }//GEN-LAST:event_formWindowClosing
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // поиск клиентов по маске
+        String text = jTextField1.getText();
+        clients = finder.findClientsByMask(clients, text);
+        refreshClientsTableModel();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
+        // сбросить фильтры и показать всех клиентов
+        this.clients = xml.readClientsFromXml(CLIENTS_FILE);
+        refreshClientsTableModel();
+    }//GEN-LAST:event_jButton7ActionPerformed
+
+    private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
+        // сбросить фильтры и показать все ордера
+        this.orders = xml.readOrdersFromXml(ORDERS_FILE);
+        refreshOrdersTableModel();
+    }//GEN-LAST:event_jButton8ActionPerformed
+
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
+        // Найти ордер по маске
+        String text = jTextField2.getText();
+        orders = finder.findOrdersByMask(orders, text);
+        refreshOrdersTableModel();
+    }//GEN-LAST:event_jButton6ActionPerformed
 
 
     public void displayView() {
@@ -299,11 +355,17 @@ public final class TestFrame extends javax.swing.JFrame {
     public Order getOrderById(long id)
     {   
         Order resultOrder = null;
+        if (Long.valueOf(id) != null) {
+
         for (Order order : orders) {
             if ( order.getOrderId() == id )
             {
                 resultOrder = order;
             }
+        }
+        }
+        else{
+            System.out.println("Получен id null");
         }
         return resultOrder;
     }
@@ -315,6 +377,8 @@ public final class TestFrame extends javax.swing.JFrame {
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTable jTable1;
